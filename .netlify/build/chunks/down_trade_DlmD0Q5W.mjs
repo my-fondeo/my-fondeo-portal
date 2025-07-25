67cdc86dae04f3a44116413e5a681a9e05696007
Juan Camilo Ayala <juanksr0@gmail.com>
2025-07-24 22:28:26 -0500

    delete netlify

--- .netlify/build/chunks/down_trade_DlmD0Q5W.mjs
+++
@@ -1,5 +0,0 @@
-import { c as createSvgComponent } from './runtime_CLftmBUo.mjs';
-
-const down_trade = createSvgComponent({"meta":{"src":"/_astro/down_trade.C7F61O1z.svg","width":50,"height":50,"format":"svg"},"attributes":{"width":"50","zoomAndPan":"magnify","viewBox":"0 0 37.5 37.499999","height":"50","preserveAspectRatio":"xMidYMid meet"},"children":"<defs><clipPath id=\"d250f1ca9f\"><path d=\"M 0.371094 3.699219 L 37.007812 3.699219 L 37.007812 28 L 0.371094 28 Z M 0.371094 3.699219 \" /></clipPath></defs><g id=\"ae73826b9f\"><g clip-rule=\"nonzero\" clip-path=\"url(#d250f1ca9f)\"><path style=\" stroke:none;fill-rule:nonzero;fill:currentColor;fill-opacity:1;\" d=\"M 33.675781 18.667969 L 33.675781 21.625 L 17.945312 5.898438 L 11.472656 12.375 L 2.960938 3.863281 L 0.371094 6.453125 L 11.472656 17.554688 L 17.945312 11.082031 L 31.085938 24.214844 L 28.125 24.214844 L 28.125 27.917969 L 37.375 27.917969 L 37.375 18.667969 L 33.675781 18.667969 \" /></g></g>"});
-
-export { down_trade as default };
